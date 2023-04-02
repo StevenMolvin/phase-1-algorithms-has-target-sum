30fb7248305daecd91da5fc40b29b896fb80004f
StevenMo <stevenmolvin@gmail.com>
2023-04-03 02:21:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // Loop through each element in the array
+  for (let i = 0; i < array.length; i++) {
+    // Loop through the remaining elements in the array
+    for (let j = i + 1; j < array.length; j++) {
+      // If the sum of the current two elements is equal to the target, return true
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  // If no two elements sum to the target, return false
+  return false;
 }
+hasTargetSum([3, 8, 12, 4, 11, 7], 10);
+hasTargetSum([22, 19, 4, 6, 30], 25);
+hasTargetSum([1, 2, 5], 4);
+hasTargetSum([-7, 10, 4, 8], 3);
+hasTargetSum([1, 2, 3, 4], 5);
+hasTargetSum([2, 2, 3, 3], 4);
+hasTargetSum([4], 5);
+
 
 /* 
   Write the Big O time complexity of your function here
